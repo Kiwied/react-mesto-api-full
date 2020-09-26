7b3d6833ada09522f6d1a9880a7d9787dde0fabd
Kiwied <anik-anton@yandex.ru>
2020-09-26 14:23:49 +0300

    fix: fronend removed from repository

--- front/src/utils/utils.js
+++
@@ -1,7 +0,0 @@
-export const apiInfo = {
-  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-12',
-  headers: {
-    authorization: '46554895-2128-4943-bcbf-ffff5703844f',
-    'Content-Type': 'application/json'
-  }
-}
