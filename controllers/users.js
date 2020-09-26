e3928c32e910fc778a291da13f300d696f4273fb
Kiwied <anik-anton@yandex.ru>
2020-09-26 18:33:03 +0300

    feat: user creates with unique email and hashed pass

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 function getUsers(req, res) {
@@ -25,16 +26,34 @@ function getUserById(req, res) {
 }
 
 function createUser(req, res) {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(400).send({ message: 'Введены некорректные данные' });
-      } else {
-        res.status(500).send({ message: 'Ошибка сервера' });
-      }
-    });
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+  User.init()
+    .then(() => {
+      bcrypt.hash(password, 10)
+        .then((hash) => {
+          User.create({
+            name,
+            about,
+            avatar,
+            email,
+            password: hash,
+          })
+            .then((user) => res.send({ data: user }))
+            .catch((err) => {
+              if (err.name === 'MongoError') {
+                res.status(400).send({ message: 'Пользователь с таким email уже зарегистрирован', one: err });
+              } else if (err.name === 'ValidationError') {
+                res.status(400).send({ message: 'Введены некорректные данные', one: err });
+              } else {
+                res.status(500).send({ message: 'Ошибка сервера' });
+              }
+            });
+        })
+        .catch((err) => res.send(err));
+    })
+    .catch((err) => res.send(err));
 }
 
 function updateProfileInfo(req, res) {
