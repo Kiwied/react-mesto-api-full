696b137887a4b838dc7a1e4973698d09210d476d
Kiwied <anik-anton@yandex.ru>
2020-09-28 12:48:34 +0300

    feat: allow CORS

--- app.js
+++ app.js
@@ -30,6 +30,14 @@ app.use(bodyParser.json());
 
 app.use(requestLogger);
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
+
+  next();
+});
+
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
